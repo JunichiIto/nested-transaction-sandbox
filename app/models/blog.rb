5dc73742fb222626afd05698e41c1e18912a7301
Junichi Ito <jit@sonicgarden.jp>
2019-06-07 20:56:00 +0900

    Add after_create callback

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,12 @@
 class Blog < ApplicationRecord
+  has_many :comments
+
+  after_create :add_initial_comment
+
+  private
+
+  def add_initial_comment
+    comment = comments.new(content: 'Great!')
+    comment.save!
+  end
 end
--- test/models/blog_test.rb
+++ test/models/blog_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class BlogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test ".after_create" do
+    blog = Blog.new(title: 'hello')
+    blog.save
+    assert blog.persisted?
+    assert blog.comments.count == 1
+  end
 end
